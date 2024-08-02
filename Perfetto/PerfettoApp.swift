e2009d75e71b9a6ef500e4a4ac812adca48102fe
azizbek <azizbek>
2024-08-02 17:36:11 +0500

    changes

---
+++ Perfetto/PerfettoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PerfettoApp.swift
+//  Perfetto
+//
+//  Created by Azizbek Musurmonov   on 02/08/24.
+//
+
+import SwiftUI
+
+@main
+struct PerfettoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
